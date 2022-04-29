718ad5c9d19cf1f61ae6f801b41c236e1b2365de
Glb <2998454039@qq.com>
2022-04-29 15:48:48 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author glb
+ * @create 2022-04-28 20:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
